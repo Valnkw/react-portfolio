7b88fa1947b1a8288bfe1b501040a123337976a3
Val <80660770+Valnkw@users.noreply.github.com>
2023-01-05 20:16:05 -0600

    go nect

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div class="dark">
+      <div
+        name="about"
+        className="w-full h-screen bg-[#f1e9da] text-gray-300 dark:bg-[#0a192f]"
+      >
+        {/*</div><div>/<div name="about" className="w-full h-screen bg-[#f1e9da] text-gray-300">*/}
+
+        <div className="flex flex-col justify-center items-center w-full h-full">
+          <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+            <div className="sm:text-right pb-8 pl-4">
+              <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+                About
+              </p>
+            </div>
+            <div></div>
+          </div>
+          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+            <div className="sm:text-right text-4xl font-bold">
+              <p>
+                Hi. I'm Valentine, nice to meet you. Please take a look around.
+              </p>
+            </div>
+            <div>
+              <p>
+                I am passionate about working with software that improves the
+                lives of those around me.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-7 flex flex-col justify-center h-full">
+        <p className="text-[#282C35] font-extrabold">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Valentine
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Nkowa</h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I’m currently a Junior at Mankato state university specializing in
+          Computer Science and Information Technology. Currently, I’m focused on
+          learning React, JS and more front-end development tools.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-stone-400 hover:border-stone-400">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
